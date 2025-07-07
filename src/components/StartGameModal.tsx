6b9188096a647ce42d1d1c240e35e3d033a6d3df
Brian <webdevbrian@gmail.com>
2025-07-07 10:51:20 -0400

    Update background to use specified image

--- src/components/StartGameModal.tsx
+++ src/components/StartGameModal.tsx
@@ -7,7 +7,7 @@ interface StartGameModalProps {
 const StartGameModal: React.FC<StartGameModalProps> = ({ onStartGame }) => {
   return (
     <div className="modal-overlay" style={{
-      backgroundImage: 'url(https://images.pexels.com/photos/355952/pexels-photo-355952.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop)',
+      backgroundImage: 'url(https://i.imgur.com/UWUfKZH.png)',
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat'
